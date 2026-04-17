acf91baede47b4728d8c225d1388e4bd33f8f529
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-04-17 14:19:57 -0500

    changes now

--- infrastructure-live/dev/s3-resource/main.tf
+++ infrastructure-live/dev/s3-resource/main.tf
@@ -7,3 +7,7 @@ module "s3" {
     ManagedBy   = "terraform"
   }
 }
+
+terraform {
+  backend "s3" {}
+}
--- infrastructure-live/dev/vpc/main.tf
+++ infrastructure-live/dev/vpc/main.tf
@@ -6,3 +6,6 @@ module "vpc" {
   subnet_cidr = "10.0.1.0/24"
   subnet_name = "dev-public-subnet"
 }
+terraform {
+  backend "s3" {}
+}
