f51585d39e7276b378932bd692811266460cd12a
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-04-17 11:56:13 -0500

    subnet changes

--- infrastructure-live/dev/vpc/main.tf
+++ infrastructure-live/dev/vpc/main.tf
@@ -2,6 +2,7 @@ module "vpc" {
   source = "../../../infrastructure/modules/vpc"
 
   vpc_cidr    = "10.0.0.0/16"
-  name        = "development-vpc"
+  vpc_name    = "development-vpc"
   subnet_cidr = "10.0.1.0/24"
+  subnet_name = "dev-public-subnet"
 }
--- infrastructure/modules/vpc/main.tf
+++ infrastructure/modules/vpc/main.tf
@@ -2,15 +2,16 @@ resource "aws_vpc" "hello" {
   cidr_block = var.vpc_cidr
 
   tags = {
-    Name = var.name
+    Name = var.vpc_name
   }
 }
 
 resource "aws_subnet" "hello" {
-  vpc_id     = aws_vpc.hello.id
-  cidr_block = var.subnet_cidr
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = var.name
+    Name = var.subnet_name
   }
 }
