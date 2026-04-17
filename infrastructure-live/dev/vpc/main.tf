f2ca6bbbf3913535eb81e81b09e78543ec1818c8
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-04-17 11:46:57 -0500

    subnet

--- infrastructure-live/dev/vpc/main.tf
+++ infrastructure-live/dev/vpc/main.tf
@@ -1,13 +1,6 @@
 module "vpc" {
   source = "../../../infrastructure/modules/vpc"
 
-  vpc_cidr = "10.0.0.0/16"
-  name     = "devlopment-vpc"
-}
-
-module "vpc" {
-  source = "../../../infrastructure/modules/vpc"
-
   vpc_cidr    = "10.0.0.0/16"
   name        = "development-vpc"
   subnet_cidr = "10.0.1.0/24"
