f6b0cee6c36b459e79a2ce68bcb9c56c4190bfdf
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-04-21 11:28:56 -0500

    s3 changes

--- infrastructure-live/dev/s3-resource/main.tf
+++ infrastructure-live/dev/s3-resource/main.tf
@@ -1,6 +1,7 @@
 module "s3" {
-  source      = "../../../infrastructure/modules/s3"
-  bucket_name = var.bucket_name
+  source        = "../../../infrastructure/modules/s3"
+  bucket_name   = var.bucket_name
+  bucket_prefix = var.bucket_prefix
   tags = {
     Environment = "dev"
     Project     = "terraform-repo"
--- infrastructure/modules/s3/main.tf
+++ infrastructure/modules/s3/main.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name
-  tags   = var.tags
+  bucket        = var.bucket_name
+  bucket_prefix = var.bucket_name == null ? var.bucket_prefix : null
+  tags          = var.tags
 }
