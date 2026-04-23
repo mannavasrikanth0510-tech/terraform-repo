f00171b40f5b180cee0c9871c7bfe94687a866f8
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-04-23 12:47:57 -0500

    s3 creation

--- infrastructure/modules/s3/main.tf
+++ infrastructure/modules/s3/main.tf
@@ -3,11 +3,3 @@ resource "aws_s3_bucket" "this" {
   bucket_prefix = var.bucket_name == null ? var.bucket_prefix : null
   tags          = var.tags
 }
-
-resource "aws_s3_bucket_versioning" "this" {
-  bucket = aws_s3_bucket.this.id
-
-  versioning_configuration {
-    status = var.versioning_enabled ? "Enabled" : "Suspended"
-  }
-}
