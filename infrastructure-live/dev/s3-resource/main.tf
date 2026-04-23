6af2a25091c1c3a773ad9d25e903d74d92780c7c
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-04-23 12:01:53 -0500

    creating

--- infrastructure-live/dev/s3-resource/main.tf
+++ infrastructure-live/dev/s3-resource/main.tf
@@ -3,6 +3,7 @@ module "s3" {
   bucket_name   = var.bucket_name
   bucket_prefix = var.bucket_prefix
   versioning_enabled = var.versioning_enabled
+  
   tags = {
     Environment = "dev"
     Project     = "terraform-repo"
